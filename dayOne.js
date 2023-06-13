f09269a33d4e101052df56dd68bc0f1b3ef8c061
Amaya Wickramasinghe <amawickramasinghe2001@gmail.com>
2023-06-13 23:10:08 +0530

    split array into multiple arrays by empty element

--- dayOne.js
+++ dayOne.js
@@ -1,45 +1,19 @@
 import * as fs from "fs";
 
 function totalOfCalories() {
-  const newTotalCaloriesArray = [];
-  const oneElveCalories = [];
-
+  //const newElveHasCalories = [];
+  //day01.txt file value inseretd an array called valuesOfCaloriesArray
   var valuesOfCalories = fs.readFileSync("day01.txt", "utf-8");
   const valuesOfCaloriesArray = valuesOfCalories.split(/\r?\n/);
-  //console.log(valuesOfCaloriesArray);
+  console.log(valuesOfCaloriesArray);
 
-  for (let i = 0; i < valuesOfCaloriesArray.length; i++) {
-    //console.log(valuesOfCaloriesArray[i]);
+  const newArray = valuesOfCaloriesArray
+    .join("#")
+    .split(/(?:^|#)()(?:#|$)/)
+    .filter(Boolean)
+    .map((v) => v.split("#"));
 
-    if (valuesOfCaloriesArray[i] != "") {
-      oneElveCalories.push(valuesOfCaloriesArray[i]);
-
-      //   var totalNumberOfCalories =
-      //     totalNumberOfCalories + valuesOfCaloriesArray[i];
-      //console.log(sumOfElement);
-
-      //break;
-    } else {
-      //   var removeSpaceElement = newTotalCaloriesArray.splice(
-      //     valuesOfCaloriesArray[i]
-      //   );
-      let sumOfElement;
-      for (let i = 0; i < oneElveCalories.length; i++) {
-        sumOfElement = sumOfElement + oneElveCalories[i];
-      }
-      newTotalCaloriesArray.push(sumOfElement);
-      //console.log(sumOfElement);
-    }
-  }
-  const filterdArray = [];
-  for (let i = 0; i < newTotalCaloriesArray.length; i++) {
-    if (newTotalCaloriesArray[i]) {
-      filterdArray.push(newTotalCaloriesArray[i]);
-    }
-  }
-  const maxCalorieTotal = Math.max(filterdArray);
-  console.log(filterdArray);
-  console.log("maxCalorieTotal:", maxCalorieTotal);
+  console.log(newArray);
 }
 
 export default totalOfCalories;
