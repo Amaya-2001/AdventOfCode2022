b2b6d349b6a817a0b4e1ae452da75f6fcbe4d505
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-06-18 00:07:27 +0530

    found the max value of calorie total

--- dayOne.js
+++ dayOne.js
@@ -1,7 +1,6 @@
 import * as fs from "fs";
 
 function totalOfCalories() {
-  var sum = 0;
   //day01.txt file value inseretd an array called valuesOfCaloriesArray
   var valuesOfCalories = fs.readFileSync("day01.txt", "utf-8");
   const valuesOfCaloriesArray = valuesOfCalories.split(/\r?\n/);
@@ -13,9 +12,14 @@ function totalOfCalories() {
     .split(/(?:^|#)()(?:#|$)/)
     .filter(Boolean)
     .map((v) => v.split("#"));
+  //console.log(newArray);
 
-  console.log(newArray);
-  //[[4,5],[6,2,3]]
+  const sum = (a, b) => Number(a) + Number(b);
+  const sumOfOneElveCalories = newArray.map((element) => element.reduce(sum));
+  console.log(sumOfOneElveCalories);
+
+  const maxCalories = Math.max(...sumOfOneElveCalories);
+  console.log(maxCalories);
 }
 
 export default totalOfCalories;
